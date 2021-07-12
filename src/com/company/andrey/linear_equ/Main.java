85684f1716da5ef320385ff5fe50befbadfcc4d8
Andrey <andrey136ru@mail.ru>
2021-07-12 11:58:24 +0300

    Решение линейного уравнения готово

--- src/com/company/andrey/linear_equ/Main.java
+++ src/com/company/andrey/linear_equ/Main.java
@@ -1,8 +1,25 @@
 package com.company.andrey.linear_equ;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите коэффициент k");
+        double k = scan.nextDouble();
+        System.out.println("Введите коэффициент b");
+        double b = scan.nextDouble();
+        if (k==0) {
+            if (b==0) {
+                System.out.println("x=любое число");
+            } else {
+                System.out.println("Решений нет");
+            }
+        } else {
+            double x = -b / k;
+            System.out.println("Ответ:");
+            System.out.println(x);
+        }
     }
 }
